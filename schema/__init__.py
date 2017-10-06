6609a1340ba630a2bafb98e011ea0fa1eabd1e14
Alaa Programmer <alaa.21.iraq@gmail.com>
2017-10-06 04:06:22 +0300

    Create __init__.py

---
+++ schema/__init__.py
@@ -0,0 +1,10 @@
+import schema.__cont_sql as schema 
+import schema.__document as docs
+from pprint import pprint
+from datetime import datetime
+
+
+date_tiem = str(datetime.now())
+time      = str(datetime.time(datetime.now()))
+date      = str(datetime.date(datetime.now()))
+
